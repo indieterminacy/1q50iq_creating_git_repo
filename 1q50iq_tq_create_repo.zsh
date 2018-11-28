3f59084f19c78d0f51240576763bc25092f5c854
Indieterminacy <indieterminacy@nortonspeal.com>
2018-11-28 15:34:34 +0100

    Provided suggestions for validations and introduced README

--- 1q50iq_tq_create_repo.zsh
+++ 1q50iq_tq_create_repo.zsh
@@ -1,11 +1,17 @@
+#!/usr/bin/zsh
+
 # Simple repo creation and committing tool
 function 1q50iq_create_repo_add_all() {
-    local memorandum
-    memorandum="$*"
+    local twt
+    # TODO Provide terminal input should message paramert be empty
+    twt="$*"
+    # TODO Add validation that git exists
     git init
     git add .
-    git commit -m "Starting repo. $memorandum"
+    git commit -m "Starting repo: $twt"
     pwd
     tree -C
+    # TODO Add validation that k exists
     k
+    # TODO Add prompt for pushing to repository
 }
