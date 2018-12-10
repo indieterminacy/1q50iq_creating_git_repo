e12eb4f60d29d4c280678bd3c5874699c626f867
Indieterminacy <indieterminacy@nortonspeal.com>
2018-12-10 15:27:55 +0100

    Updated some of the descriptive content

--- 1q50iq_tq_create_repo.zsh
+++ 1q50iq_tq_create_repo.zsh
@@ -3,12 +3,13 @@
 # Simple repo creation and committing tool
 function 1q50iq_create_repo_add_all() {
     local twt
-    # TODO Provide terminal input should message paramert be empty
+    # TODO Provide terminal input should the message parameter be empty
     twt="$*"
     # TODO Add validation that git exists
     git init
     git add .
     git commit -m "Starting repo: $twt"
+
     pwd
     tree -C
     # TODO Add validation that k exists
